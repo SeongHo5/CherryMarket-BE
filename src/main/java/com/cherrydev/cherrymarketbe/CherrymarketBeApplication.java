408479e4b50ecaabf8c384e7037d1ff8b42f45cb
SeongHo5 <newfuring1123@gmail.com>
2023-11-26 17:30:04 +0900

    build: Set up project structure
    - 프로젝트 의존성 설정
    - 커밋 메세지 컨벤션 관련 문구 추가

---
+++ src/main/java/com/cherrydev/cherrymarketbe/CherrymarketBeApplication.java
@@ -0,0 +1,32 @@
+package com.cherrydev.cherrymarketbe;
+
+import jakarta.annotation.PostConstruct;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
+
+import static com.cherrydev.cherrymarketbe.common.log.CherryLogger.logServerStart;
+
+@EnableScheduling
+@SpringBootApplication
+@MapperScan(basePackages = "com.cherrydev.cherrymarketbe")
+public class CherrymarketBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherrymarketBeApplication.class, args);
+		logServerStart();
+	}
+
+	/**
+	 * TimeZone을 한국 시간으로 설정
+	 */
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+
+}
---
+++ src/test/java/com/cherrydev/cherrymarketbe/CherrymarketBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherrydev.cherrymarketbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherrymarketBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
