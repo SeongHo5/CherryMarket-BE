eef613e19f17837ccaf9a4248136010ad006303d
SeongHo5 <newfuring1123@gmail.com>
2023-11-27 12:13:34 +0900

    chore: Clean up unused import and comments

--- src/main/java/com/cherrydev/cherrymarketbe/common/utils/CodeGenerator.java
+++ src/main/java/com/cherrydev/cherrymarketbe/common/utils/CodeGenerator.java
@@ -15,6 +15,7 @@ public class CodeGenerator {
 
     /**
      * SecureRandom을 이용하여 무작위 코드를 생성합니다.
+     * @param requiredLength 생성할 코드의 길이
      */
     public static String generateRandomCode(final int requiredLength) {
         StringBuilder sb = new StringBuilder(requiredLength);
