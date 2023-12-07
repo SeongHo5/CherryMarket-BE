a3432d861042ef0440028e2478de6374ec3a75d7
ssomal62 <wxy890@gmail.com>
2023-12-07 23:21:54 +0900

    refactor: Refactor cart code

---
+++ src/main/java/com/cherrydev/cherrymarketbe/cart/dto/CartsByStorageType.java
@@ -0,0 +1,22 @@
+package com.cherrydev.cherrymarketbe.cart.dto;
+
+import com.cherrydev.cherrymarketbe.cart.entity.Cart;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public record CartsByStorageType(
+        Map<String, List<CartDetails>> itemsByType
+) {
+
+    public CartsByStorageType(List<Cart> carts) {
+        this(
+                carts.stream()
+                        .filter(Cart::isGoodsAvailable)
+                        .map(CartDetails::getCarts)
+                        .collect(Collectors.groupingBy(CartDetails::storageType))
+        );
+    }
+
+}
