02e11483c7add98e8d6e4aff113f5bf4979976cd
ssomal62 <wxy890@gmail.com>
2023-12-08 10:29:52 +0900

    refactor: Refactor cart code
    - Builder 처리를 어노테이션으로 전환

--- src/main/java/com/cherrydev/cherrymarketbe/cart/entity/Cart.java
+++ src/main/java/com/cherrydev/cherrymarketbe/cart/entity/Cart.java
@@ -3,6 +3,7 @@ package com.cherrydev.cherrymarketbe.cart.entity;
 import com.cherrydev.cherrymarketbe.goods.entity.Goods;
 import com.cherrydev.cherrymarketbe.goods.enums.SalesStatus;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,69 +15,16 @@ public class Cart {
 
     private Long accountId;
 
-    private int quantity;
+    private Integer quantity;
 
     private Goods goods;
 
-    public static class CartUpdateBuilder {
-        private Long cartId;
-        private Integer quantity;
-
-        public CartUpdateBuilder cartId(Long cartId) {
-            this.cartId = cartId;
-            return this;
-        }
-
-        public CartUpdateBuilder quantity(int quantity) {
-            this.quantity = quantity;
-            return this;
-        }
-
-        public Cart build() {
-            Cart cart = new Cart();
-            cart.cartId = this.cartId;
-            cart.quantity = this.quantity;
-
-            return cart;
-        }
-    }
-
-    public static class CartCreateBuilder {
-        private Long accountId;
-        private Integer quantity;
-        private Goods goods;
-
-        public CartCreateBuilder accountId(Long accountId) {
-            this.accountId = accountId;
-            return this;
-        }
-
-        public CartCreateBuilder quantity(int quantity) {
-            this.quantity = quantity;
-            return this;
-        }
-
-        public CartCreateBuilder goods(Goods goods) {
-            this.goods = goods;
-            return this;
-        }
-
-        public Cart build() {
-            Cart cart = new Cart();
-            cart.accountId = this.accountId;
-            cart.quantity = this.quantity;
-            cart.goods = this.goods;
-
-            return cart;
-        }
-    }
-
-    public static CartUpdateBuilder builderUpdate() {
-        return new CartUpdateBuilder();
-    }
-
-    public static CartCreateBuilder builderCreate() {
-        return new CartCreateBuilder();
+    @Builder
+    public Cart(Long cartId, Long accountId, Integer quantity, Goods goods) {
+        this.cartId = cartId;
+        this.accountId = accountId;
+        this.quantity = quantity;
+        this.goods = goods;
     }
 
     public boolean isGoodsAvailable() {
