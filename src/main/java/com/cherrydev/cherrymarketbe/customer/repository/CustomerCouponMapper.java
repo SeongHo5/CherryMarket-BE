aa5c37b5a22ef77be2ff5b4eee4e5399ff09817d
SeongHo5 <newfuring1123@gmail.com>
2023-12-11 14:45:44 +0900

    refactor: Rename DTO class

--- src/main/java/com/cherrydev/cherrymarketbe/customer/repository/CustomerCouponMapper.java
+++ src/main/java/com/cherrydev/cherrymarketbe/customer/repository/CustomerCouponMapper.java
@@ -3,10 +3,14 @@ package com.cherrydev.cherrymarketbe.customer.repository;
 import com.cherrydev.cherrymarketbe.customer.entity.CustomerCoupon;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface CustomerCouponMapper {
 
     void save(CustomerCoupon customerCoupon);
+
+    List<CustomerCoupon> findAllByAccountId(Long accountId);
 
     boolean existByCouponIdAndAccountId(Long couponId, Long accountId);
 
